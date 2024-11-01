f0130ad0870b3b15942758049436396dce8a0fe2
Serhii Bolhovskyi <serhiibolhovskyi@MacBook-Air-Serhii.local>
2024-11-01 17:30:16 +0100

    Created new project for solution

---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter the value of m: "); double.TryParse(Console.ReadLine(), out double m);
+Console.WriteLine("Enter the value of n: "); double.TryParse(Console.ReadLine(), out double n);
+
+double z1, z2;
+z1 = ((m - 1) * Math.Log(m) - (n - 1) * Math.Log(n)) / (Math.Log(Math.Pow(m, 3) * n) + n*m + m*m - m);
+z2 = (Math.Log(m) - Math.Log(n)) / m;
+Console.WriteLine($"The value of z1: {z1}, z2: {z2}");
