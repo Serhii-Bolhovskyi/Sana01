a62d7488b9ff962215f94fae3cf1e8858a37c182
Serhii Bolhovskyi <serhiibolhovskyi@MacBook-Air-Serhii.local>
2024-11-01 17:31:46 +0100

    Updated code, made it more readable and logical

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -1,11 +1,11 @@
-﻿Console.WriteLine("Enter the value of a: "); double a = double.Parse(Console.ReadLine());
-Console.WriteLine("Enter the value of b: "); double b = double.Parse(Console.ReadLine());
-Console.WriteLine("Enter the value of c: "); double c = double.Parse(Console.ReadLine());
-Console.WriteLine("Enter the value of d: "); double d = double.Parse(Console.ReadLine());
+﻿Console.WriteLine("Enter the value of a: "); double.TryParse(Console.ReadLine(), out double a);
+Console.WriteLine("Enter the value of b: "); double.TryParse(Console.ReadLine(), out double b);
+Console.WriteLine("Enter the value of c: "); double.TryParse(Console.ReadLine(), out double c);
+Console.WriteLine("Enter the value of d: "); double.TryParse(Console.ReadLine(), out double d);
 
 double x, y, z, r;
 x = ((a + 2*b - c + d) / (c*d)) + ((a + b)/(c - d)) - (Math.Pow(a, 2)/Math.Pow(b, 2));
 y = 5 * (a + b) * (c - d) / ((1 / 2) * c) + double.Pow(b, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2)) / (b-a));
 z = (Math.Pow(x*x - 2*x, 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b) / (5*a + 3*b);
 r = (1/2 * a + 3/4 * b + 7/5) / (3*c + 1) + 1 / (a - c);
-Console.WriteLine("x = {0}, y = {1}, z = {2}, r = {3}", x, y, z, r);
+Console.WriteLine($"x = {x}, y = {y}, z = {z}, r = {r}");
